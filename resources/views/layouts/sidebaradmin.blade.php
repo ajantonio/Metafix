62d9a924f3e8205c3a3fa1feae1f3182887fa4d8
ajantonio <ajantonio01@yahoo.com>
2021-10-18 19:03:42 +0800

    Added new links for inventory and technicians management modules
    There are 2 new modules added according to our client's request for MVP: 'Add Orthopedic Technicians' and 'View Orthopedic Technicians'. 'Check Orthopedic Implants' module name changed to 'View Inventory'

--- resources/views/layouts/sidebaradmin.blade.php
+++ resources/views/layouts/sidebaradmin.blade.php
@@ -1,32 +1,41 @@
 <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                    <div class="nav">
-                            <div class="sb-sidenav-menu-heading text-white">Manage Accounts</div>
-                            <a class="nav-link" href="">
-                                <div class="sb-nav-link-icon"></div>
-                                View Accounts
-                            </a>
-                            <div class="sb-sidenav-menu-heading text-white">Manage Transactions</div>
-                            <a class="nav-link" href="">
-                                <div class="sb-nav-link-icon"></div>
-                                Add Quotation
-                            </a>
-                            <a class="nav-link" href="">
-                                <div class="sb-nav-link-icon"></div>
-                                Check History Transactions
-                            </a>
-                            <div class="sb-sidenav-menu-heading text-white">Maintenance</div>
-                            <a class="nav-link" href="">
-                                <div class="sb-nav-link-icon"></div>
-                                Add Orthopedic Implants
-                            </a>
-                            <a class="nav-link" href="">
-                                <div class="sb-nav-link-icon"></div>
-                                Check Inventory
-                            </a>
-                        </div>
-                    </div>
-
-                </nav>
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading text-white">Manage User Accounts</div>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"></div>
+                    View Accounts
+                </a>
+                <div class="sb-sidenav-menu-heading text-white">Manage Transactions</div>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"></div>
+                    Add Quotation
+                </a>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"></div>
+                    Check History Transactions
+                </a>
+                <div class="sb-sidenav-menu-heading text-white">Inventory Management</div>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"></div>
+                    Add Orthopedic Implants
+                </a>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"></div>
+                    View Inventory
+                </a>
+                <div class="sb-sidenav-menu-heading text-white">Technicians Management</div>
+                <a class="nav-link" href="{{ route('addorthopedictechnicians.home') }}">
+                    <div class="sb-nav-link-icon"></div>
+                    Add Orthopedic Technicians
+                </a>
+                <a class="nav-link" href="{{ route('vieworthopedictechnicians.home') }}">
+                    <div class="sb-nav-link-icon"></div>
+                    View Orthopedic Technicians
+                </a>
             </div>
+        </div>
+
+    </nav>
+</div>
