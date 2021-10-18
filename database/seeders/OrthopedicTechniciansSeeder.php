a0307580d7e643b611a8e4b8bb4772f4ed812637
ajantonio <ajantonio01@yahoo.com>
2021-10-18 19:09:49 +0800

    New manually-typed seeder for 'orthopedic_technicians' table

--- database/seeders/OrthopedicTechniciansSeeder.php
+++ database/seeders/OrthopedicTechniciansSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\OrthopedicTechnician;
+use Illuminate\Support\Facades\Storage;
+
 
 class OrthopedicTechniciansSeeder extends Seeder
 {
@@ -14,6 +16,43 @@ class OrthopedicTechniciansSeeder extends Seeder
      */
     public function run()
     {
-        OrthopedicTechnician::factory()->times(5)->create();
+        OrthopedicTechnician::firstOrCreate([
+            'name' => 'Martin Leewunherok',
+            'image' => 'public/images/technician-1.jpg',
+            'contact_number' => '09946135619',
+            'status' => 'On hold'
+        ]);
+
+
+        OrthopedicTechnician::firstOrCreate([
+            'name' => 'Big Van Zhong',
+            'image' => 'public/images/technician-2.jpg',
+            'contact_number' => '09748376839',
+            'status' => 'On hold'
+        ]);
+
+
+        OrthopedicTechnician::firstOrCreate([
+            'name' => 'Harry Gonzales',
+            'image' => 'public/images/technician-3.jpg',
+            'contact_number' => '09760183547',
+            'status' => 'On hold'
+        ]);
+
+
+        OrthopedicTechnician::firstOrCreate([
+            'name' => 'John Smithers',
+            'image' => 'public/images/technician-4.jpg',
+            'contact_number' => '09916734657',
+            'status' => 'On hold'
+        ]);
+
+
+        OrthopedicTechnician::firstOrCreate([
+            'name' => 'Val Menesez',
+            'image' => 'public/images/technician-5.jpg',
+            'contact_number' => '09017384567',
+            'status' => 'On hold'
+        ]);
     }
 }
