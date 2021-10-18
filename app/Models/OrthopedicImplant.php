9cc80fd0c4c970a7246afede224a48ede9eace00
ajantonio <ajantonio01@yahoo.com>
2021-10-18 08:46:36 +0800

    Added columns as fillable

--- app/Models/OrthopedicImplant.php
+++ app/Models/OrthopedicImplant.php
@@ -11,22 +11,27 @@ use App\Models\PlateType;
 
 class OrthopedicImplant extends Model
 {
-    protected $fillable = ['screw_types_id', 'screw_grades_id', 'screw_price', 'plate_types_id', 'plate_grades_id', 'plate_price'
-    , 'diameter', 'length'];
+    protected $fillable = [
+        'name', 'image', 'description', 'screw_types_id', 'screw_grades_id', 'price', 'plate_types_id', 'plate_grades_id', 'diameter', 'length'
+    ];
 
-    public function plate_grades(){
+    public function plate_grades()
+    {
         return $this->hasMany(PlateGrade::class, 'id', 'plate_grades_id');
     }
 
-    public function screw_grades(){
+    public function screw_grades()
+    {
         return $this->hasMany(ScrewGrade::class, 'id', 'screw_grades_id');
     }
 
-    public function screw_types(){
+    public function screw_types()
+    {
         return $this->hasMany(ScrewType::class, 'id', 'screw_types_id');
     }
 
-    public function plate_types(){
+    public function plate_types()
+    {
         return $this->hasMany(PlateType::class, 'id', 'plate_types_id');
     }
 
